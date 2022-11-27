0de7b033f7b1762263d2de2d240ccd7d02beaeb4
Thuan Vu <thuanvd.uit@gmail.com>
2022-11-27 12:51:44 +0700

    update

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'nev65097_nevafarm' );
+define( 'DB_NAME', 'mat21195_matkinhminhhieu' );
 
 /** Database username */
-define( 'DB_USER', 'nev65097_nevafarm' );
+define( 'DB_USER', 'mat21195_matkinhminhhieu' );
 
 /** Database password */
-define( 'DB_PASSWORD', '2qY2@w00r' );
+define( 'DB_PASSWORD', 'r9dNe_425' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
